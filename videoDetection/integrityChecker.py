1a1f92a15fc52047d65f5e6569a170f6cba52502
Ubuntu <anto@TFM.xuqxeykyz2sujfuayjgdzwzbqc.ax.internal.cloudapp.net>
2021-02-10 18:22:46 +0000

    up

--- videoDetection/integrityChecker.py
+++ videoDetection/integrityChecker.py
@@ -3,19 +3,23 @@ from PIL import Image
 import os
 
 dataset = "./cleaned/"
+
 train_path = dataset + "train_frames/"
 val_path = dataset + "val_frames/"
 test_path = dataset + "test_frames/"
+sets = [train_path, val_path, test_path]
 
-emotions = os.listdir(train_path)
-
-for emotion in emotions:
-    frames = os.listdir(test_path+emotion)
+for dataset in sets:
+  emotions = os.listdir(dataset)
+  for emotion in emotions:
+    frames = os.listdir(train_path+emotion)
+    i=0
     for frame in frames:
       if frame.endswith('.bmp'):
         try:
-          img = Image.open('./'+frame) # open the image file
+          img = Image.open(train_path+emotion + "/" + frame) # open the image file
           img.verify() # verify that it is, in fact an image
         except (IOError, SyntaxError) as e:
-          print('Bad file:', frame) # print out the names of corrupt files
-          os.remove(test_path+emotion+"/"+frame)
+          print('Bad file:', frame, e) # print out the names of corrupt files
+          i+=1
+          print("Frame: " + frame + " is the number " + i +" corrupted frame")
