74492bf552d3ead7c0002757766f17cb78785fe2
Ubuntu <anto@TFM.xuqxeykyz2sujfuayjgdzwzbqc.ax.internal.cloudapp.net>
2021-02-11 10:25:05 +0000

    up

--- videoDetection/metadataChecker.py
+++ videoDetection/metadataChecker.py
@@ -1,6 +1,7 @@
 from os import listdir
 from PIL import Image
 import os
+import cv2
 
 dataset = "./cleaned/"
 
@@ -9,17 +10,19 @@ val_path = dataset + "val_frames/"
 test_path = dataset + "test_frames/"
 sets = [train_path, val_path, test_path]
 
+i=0
 for dataset in sets:
   emotions = os.listdir(dataset)
   for emotion in emotions:
     frames = os.listdir(train_path+emotion)
-    i=0
     for frame in frames:
       if frame.endswith('.bmp'):
+        img = cv2.imread(train_path+emotion + "/" + frame) # open the image file
         try:
-          img = Image.open(train_path+emotion + "/" + frame) # open the image file
-          img.verify() # verify that it is, in fact an image
-        except (IOError, SyntaxError) as e:
-          print('Bad file:', frame, e) # print out the names of corrupt files
+          dummy = img.shape
+        except:
           i+=1
-          print("Frame: " + frame + " is the number " + i +" corrupted frame")
+          print("Frame: " + frame + " is the number " + str(i) + " corrupted frame")
+          print("removing: " + train_path+emotion+ "/" + frame)
+          os.remove(train_path+emotion+ "/" + frame)
+
