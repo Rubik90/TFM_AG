074526345d76c4654fc3cdab1eeef64096929f83
Ubuntu <anto@TFM.xuqxeykyz2sujfuayjgdzwzbqc.ax.internal.cloudapp.net>
2021-02-13 12:19:45 +0000

    up

--- videoDetection/emotion_classifiers/classifiers/conv2daffwild.py
+++ videoDetection/emotion_classifiers/classifiers/conv2daffwild.py
@@ -29,9 +29,9 @@ from tensorflow.keras import preprocessing, datasets, layers, models
 from tensorflow.keras.applications.resnet50 import ResNet50
 
 
-train_path = "../cleaned/train_frames"
-val_path = "../cleaned/val_frames"
-test_path = "../cleaned/test_frames"
+train_path = "../datasets/cleaned/train_frames"
+val_path = "../datasets/cleaned/val_frames"
+test_path = "../datasets/cleaned/test_frames"
 
 IMG_HEIGHT = 112
 IMG_WIDTH = 112
