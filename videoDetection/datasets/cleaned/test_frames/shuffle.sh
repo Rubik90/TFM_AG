e8aaa9a9c91df8da6381b5be6086a8d5c70551fb
Rubik90 <31125315+Rubik90@users.noreply.github.com>
2021-02-14 10:28:46 +0100

    up

--- videoDetection/datasets/cleaned/test_frames/shuffle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for frame in "$1"/*; do
-    name=$(head /dev/urandom | tr -dc a-z0-9 | head -c 8)
-    base=${frame%.*}
-    ext=${frame#$base.}
-    cp "$frame" "$2"/"$name"."$ext"
-done
