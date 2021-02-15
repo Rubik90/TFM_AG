3f84f393f1e03f8c4fe74c101760411e531bd743
Ubuntu <anto@TFM.xuqxeykyz2sujfuayjgdzwzbqc.ax.internal.cloudapp.net>
2021-02-15 12:21:25 +0000

    up

--- audioDetection/config.py
+++ audioDetection/config.py
@@ -3,7 +3,7 @@ import pathlib
 
 working_dir_path = pathlib.Path().absolute()
 
-TRAINING_FILES_PATH = str(working_dir_path) + '/RAVDESS_TESS_data/'
+TRAINING_FILES_PATH = str(working_dir_path) + '/RAVDESS_TESS_dataset/'
 SAVE_DIR_PATH = str(working_dir_path) + '/joblib_features/'
 MODEL_DIR_PATH = str(working_dir_path) + '/model/'
 TESS_ORIGINAL_FOLDER_PATH = str(working_dir_path) + '/TESS_Toronto_emotional_speech_set_data/'
